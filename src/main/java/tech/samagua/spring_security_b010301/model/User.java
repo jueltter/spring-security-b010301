3b6c16813cee8a8f5b9e53895a288c0632d52ac0
Stalin Amagua <jueltter@users.noreply.github.com>
2026-02-04 00:22:26 -0500

    - dependencies added - user model added

---
+++ src/main/java/tech/samagua/spring_security_b010301/model/User.java
@@ -0,0 +1,43 @@
+package tech.samagua.spring_security_b010301.model;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Getter
+public class User implements UserDetails {
+
+    private final String username;
+    private final String password;
+    private final String authority;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(() -> authority);
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
