ede89c4f9b60169b9f911fa23c4c29d03c80c67e
Stalin Amagua <jueltter@users.noreply.github.com>
2026-02-03 23:47:01 -0500

    initial commit

---
+++ src/test/java/tech/samagua/spring_security_b010301/SpringSecurityB010301ApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.samagua.spring_security_b010301;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityB010301ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
