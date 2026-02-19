f33ee2da95626412534bcc088f424a99c54460d9
Stalin Amagua <jueltter@users.noreply.github.com>
2026-02-18 22:23:11 -0500

    SecurityUser.java: added User.java: updated

---
+++ src/main/java/tech/samagua/spring_security_b010301/model/SecurityUser.java
@@ -0,0 +1,30 @@
+package tech.samagua.spring_security_b010301.model;
+
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@RequiredArgsConstructor
+public class SecurityUser implements UserDetails {
+
+    private final User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(user::getAuthority);
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+}
--- src/main/java/tech/samagua/spring_security_b010301/model/User.java
+++ src/main/java/tech/samagua/spring_security_b010301/model/User.java
@@ -1,43 +1,26 @@
 package tech.samagua.spring_security_b010301.model;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.List;
 
-@RequiredArgsConstructor
-@Getter
-public class User implements UserDetails {
+@Entity
+@Table(name = "account")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
 
-    private final String username;
-    private final String password;
-    private final String authority;
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(() -> authority);
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
+    @Id
+    private int id;
+    private String username;
+    private String password;
+    private String authority;
 }
