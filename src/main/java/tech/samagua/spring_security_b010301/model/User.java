6b899118dbbdf8beaa78003ac443d4db27ac23d5
Stalin Amagua <jueltter@users.noreply.github.com>
2026-02-18 22:35:09 -0500

    SecurityUser.java: deleted User.java: updated

--- src/main/java/tech/samagua/spring_security_b010301/model/User.java
+++ src/main/java/tech/samagua/spring_security_b010301/model/User.java
@@ -1,8 +1,5 @@
 package tech.samagua.spring_security_b010301.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -10,17 +7,36 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.util.Collection;
 import java.util.List;
 
-@Entity
-@Table(name = "account")
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
-public class User {
+@RequiredArgsConstructor
+@Getter
+public class User implements UserDetails {
 
-    @Id
-    private int id;
-    private String username;
-    private String password;
-    private String authority;
+    private final String username;
+    private final String password;
+    private final String authority;
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(() -> authority);
+    }
 }
